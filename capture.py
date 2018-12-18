609f244b13c93e301f76cb9d58af39bc122d5418
jyore <joey@jyore.com>
2018-12-17 18:26:41 -0600

    poc

---
+++ capture.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os, unicodedata,uuid
+from aqt import mw
+from aqt.qt import *
+from aqt.webview import AnkiWebView
+from aqt.utils import showInfo
+from io import BytesIO
+from PIL import Image, ImageChops
+
+
+
+def convert(image):
+  buf = QBuffer()
+  buf.open(QBuffer.ReadWrite)
+  image.save(buf, "png")
+  return Image.open(BytesIO(str(buf.data())))
+
+
+def trim(image):
+  bg = Image.new(image.mode, image.size, image.getpixel((0,0)))
+  diff = ImageChops.difference(image, bg)
+  diff = ImageChops.add(diff, diff, 2.0, -100)
+  bbox = diff.getbbox()
+  if bbox:
+      return image.crop(bbox)
+  else:
+      return image
+
+
+def generate(expression, cb=None):
+  web = AnkiWebView()
+
+  def save_image():
+    page = web.page()
+    page.setViewportSize(page.mainFrame().contentsSize())
+
+    image = QImage(page.viewportSize(), QImage.Format_ARGB32)
+    painter = QPainter(image)
+    page.mainFrame().render(painter)
+    painter.end()
+
+    if cb:
+      cb(trim(convert(image)))
+
+
+  def print_page():
+    web.loadFinished.disconnect()
+
+    dom = web.page().mainFrame()
+    dom.evaluateJavaScript('document.getElementById("PhrasingPublishIndexForm").target = ""')
+    dom.evaluateJavaScript('document.querySelector(\'#PhrasingPublishIndexForm input[type="submit"]\').click()')
+
+    web.loadFinished.connect(lambda: QTimer.singleShot(100, lambda: save_image()))
+
+
+  def submit_text():
+    web.loadFinished.disconnect()
+
+    dom = web.page().mainFrame()
+    dom.evaluateJavaScript('document.getElementById("PhrasingText").value = "%s"' % expression)
+    dom.evaluateJavaScript('document.getElementById("PhrasingIndexForm").submit()')
+
+    web.loadFinished.connect(print_page)
+
+
+  web.load(QUrl("http://www.gavo.t.u-tokyo.ac.jp/ojad/phrasing/index"))
+  web.loadFinished.connect(submit_text)
+
+
+
+class PA_Web:
+
+  def __init__(self, mw):
+    self.run_btn = QAction("Test Pitch Accent Image", mw)
+    mw.connect(self.run_btn, SIGNAL("triggered()"), self.setup)
+    mw.form.menuTools.addAction(self.run_btn)
+
+
+  def setup(self):
+    generate("あそこに立ってる人、もしかして部長!?", self.result)
+
+
+  def result(self, image):
+    fn = "%s.png" % uuid.uuid4()
+    image.save(fn) 
+    showInfo(fn)
+
+mw.webpage = PA_Web(mw)
